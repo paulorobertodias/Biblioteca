f538cdef90102f4f33d68acc013f9452ea3882df
paulorobertodias <102977326+paulorobertodias@users.noreply.github.com>
2022-04-08 09:18:35 -0300

    Altera Emprestimo Livros

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -30,14 +30,63 @@ namespace Biblioteca.Models
             }
         }
 
-        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
+        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro = null)// Filtro não utilizado
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                IQueryable<Emprestimo> consulta;
+                
+                if(filtro != null)
+                {
+                    //definindo dinamicamente a filtragem
+                    switch(filtro.TipoFiltro)
+                    {
+                        case "Usuário":
+                            consulta = bc.Emprestimos.Where(e => e.NomeUsuario.Contains(filtro.Filtro));
+                        break;
+
+                        case "Livro":
+                             //consulta = bc.Emprestimos.Where(e => bc.Livros Find(e.LivroId).Titulo.Contains(filtro.Filtro)); NÃO FUNCIONA
+                             List<Livro> LivrosFiltrados = bc.Livros.Where(l => l.Titulo.Contains(filtro.Filtro)).ToList();
+
+                             List<int> LivrosIds = new List<int>();
+                             for (int i = 0; i < LivrosFiltrados.Count; i++)
+                             {
+                                 LivrosIds.Add(LivrosFiltrados[i].Id);
+                             }
+                             consulta = bc.Emprestimos.Where(e => LivrosIds.Contains(e.LivroId));
+                             var debug = consulta.ToList();
+                        break;
+
+                        default:
+                            consulta = bc.Emprestimos;
+                        break;
+                    }
+                }
+                else
+                {
+                    // caso filtro não tenha sido informado
+                    consulta = bc.Emprestimos;
+                }
+                
+                List<Emprestimo> ListaCosulta = consulta.OrderByDescending(e => e.DataDevolucao).ToList();
+
+                for (int i = 0; i < ListaCosulta.Count; i++)
+                {
+                    ListaCosulta[i].Livro = bc.Livros.Find(ListaCosulta[i].LivroId);
+                }
+                //ordenação padrão
+                return ListaCosulta;
+            }
+        }
+/*        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
         {
             using(BibliotecaContext bc = new BibliotecaContext())
             {
                 return bc.Emprestimos.Include(e => e.Livro).ToList();
             }
         }
-
+*/
         public Emprestimo ObterPorId(int id)
         {
             using(BibliotecaContext bc = new BibliotecaContext())
