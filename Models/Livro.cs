a7d917ce5cccf68c2bd4aa4c0f70a2ed2761c782
paulorobertodias <102977326+paulorobertodias@users.noreply.github.com>
2022-04-14 15:36:26 -0300

    Alterado Livro

--- Models/Livro.cs
+++ Models/Livro.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 namespace Biblioteca.Models
 {
     public class Livro
     {
         public int Id { get; set; }
+        [Required]
         public string Titulo { get; set; }
+        [Required]
         public string Autor { get; set; }
         public int Ano { get; set; }
     }
